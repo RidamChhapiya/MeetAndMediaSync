7592e191d2cc50c6703a6d54c4c307da1ca444f1
RidamChhapiya <ridam.chhapiya@searce.com>
2025-03-26 11:12:24 +0530

    changed config

--- backend/src/config/gcpConfig.ts
+++ backend/src/config/gcpConfig.ts
@@ -1,11 +1,11 @@
 import { Storage } from "@google-cloud/storage";
 import path from "path";
 
-const storage = new Storage({
-  keyFilename: path.join(__dirname, process.env.GCP_SERVICE_KEY_PATH ?? ""), 
-  projectId: process.env.GCP_PROJECT_ID,
-});
-
+// const storage = new Storage({
+//   keyFilename: path.join(__dirname, process.env.GCP_SERVICE_KEY_PATH ?? ""), 
+//   projectId: process.env.GCP_PROJECT_ID,
+// });
+const storage = new Storage();
 const bucketName = process.env.GCP_BUCKET_NAME ?? "cloudcapture-bucket";
 
 export { storage, bucketName };
